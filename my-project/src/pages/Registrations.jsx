b4319097c40766edc35469e9e8c14306d9d34314
DaliaHamed <hameddalia204@gmail.com>
2024-08-20 21:46:39 +0300

    upd23

--- my-project/src/pages/Registrations.jsx
+++ my-project/src/pages/Registrations.jsx
@@ -1,24 +1,141 @@
+// // eslint-disable-next-line no-unused-vars
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom'; // Import Link and useNavigate
+
+// function Contact() {
+//   const navigate = useNavigate(); // Use useNavigate for navigation
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault(); // Prevent page reload on form submission
+
+//     // Navigate to "Credentials" page on form submit
+//     navigate('/Groups'); // Navigate to credentials page
+//   };
+
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: 'url("cer4.jpg")' }}>
+//       <div className="bg-white bg-opacity-50 p-8 rounded-3xl shadow-lg w-full max-w-md border-4 border-green-600"> {/* Added border width and color */}
+//         <h1 className="text-3xl font-semibold mb-6 text-center text-green-700">LOG IN</h1>
+//         <form className="space-y-6" onSubmit={handleSubmit}> {/* Increased spacing */}
+//           <div>
+//             <label htmlFor="Username" className="block text-sm font-medium text-gray-700 pl-2"> {/* Larger font size */}
+//               Username
+//             </label>
+//             <input
+//               type="text"
+//               id="Username"
+//               name="Username"
+//               className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               required
+//             />
+//           </div>
+//           <div>
+//             <label htmlFor="Password" className="block text-sm font-medium text-gray-700 pl-2">
+//               Password
+//             </label>
+//             <input
+//               type="password"
+//               id="Password"
+//               name="Password"
+//               className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               required
+//             />
+//           </div>
+//           <div className="flex items-center gap-2">
+//             <input
+//               type="checkbox"
+//               id="checkbox"
+//               name="checkbox"
+//               className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
+//             />
+//             <label htmlFor="checkbox" className="text-sm text-gray-700">
+//               Remember me
+//             </label>
+//           </div>
+//           <button
+//             type="submit"
+//             className="w-full py-3 px-4 bg-green-600 text-white font-semibold rounded-lg shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+//           >
+//             Submit
+//           </button>
+//           <div className="text-center text-sm text-gray-700">
+//             Don’t have an account? <Link to="/SignUp" className="text-green-600 hover:text-green-500">Sign up</Link>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Contact;
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Import Link and useNavigate
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Contact() {
-  const navigate = useNavigate(); // Use useNavigate for navigation
+  const navigate = useNavigate();
+  
+  // State to store messages
+  const [message, setMessage] = useState(null);
+  const [messageType, setMessageType] = useState(''); // 'success' or 'error'
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent page reload on form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    // Navigate to "Credentials" page on form submit
-    navigate('/Groups'); // Navigate to credentials page
+    // Gather form data
+    const formData = new FormData(e.target);
+    const data = {
+      adminEmail: formData.get('Username'), // Email field
+      adminPassword: formData.get('Password') // Password field
+    };
+
+    // Send data to the PHP backend
+    try {
+      const response = await fetch('https://certificatemakerapp-com.stackstaging.com/api/ourProject/login.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'  // Set content type as JSON
+        },
+        body: JSON.stringify(data)  // Send the data as JSON
+      });
+      
+      const result = await response.json();
+
+      if (result.status === 'success') {
+        // Set success message and navigate to the /Groups page
+        setMessage("Login successful!");
+        setMessageType('success');
+        setTimeout(() => {
+          navigate('/Groups');
+        }, 1000); // Redirect after 1 second
+      } else {
+        // Set error message from the response
+        setMessage(result.message);
+        setMessageType('error');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle fetch error
+      setMessage("An error occurred during login. Please try again.");
+      setMessageType('error');
+    }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: 'url("cer4.jpg")' }}>
-      <div className="bg-white bg-opacity-50 p-8 rounded-3xl shadow-lg w-full max-w-md border-4 border-green-600"> {/* Added border width and color */}
+      <div className="bg-white bg-opacity-50 p-8 rounded-3xl shadow-lg w-full max-w-md border-4 border-green-600">
         <h1 className="text-3xl font-semibold mb-6 text-center text-green-700">LOG IN</h1>
-        <form className="space-y-6" onSubmit={handleSubmit}> {/* Increased spacing */}
+
+        {/* Show message if exists */}
+        {message && (
+          <div className={`p-4 mb-6 text-white ${messageType === 'success' ? 'bg-green-500' : 'bg-red-500'} rounded-lg`}>
+            {message}
+          </div>
+        )}
+
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="Username" className="block text-sm font-medium text-gray-700 pl-2"> {/* Larger font size */}
+            <label htmlFor="Username" className="block text-sm font-medium text-gray-700 pl-2">
               Username
             </label>
             <input
--- my-project/src/pages/SignUp.jsx
+++ my-project/src/pages/SignUp.jsx
@@ -1,3 +1,164 @@
+// // eslint-disable-next-line no-unused-vars
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// const SignUp = () => {
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [confirmPassword, setConfirmPassword] = useState('');
+//   const [gender, setGender] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+    
+//     // Check if all fields are filled
+//     if (!firstName || !lastName || !email || !password || !confirmPassword || !gender) {
+//       setErrorMessage('Please fill out all fields.');
+//       return;
+//     }
+
+//     // Check if password and confirm password match
+//     if (password !== confirmPassword) {
+//       setErrorMessage('Passwords do not match.');
+//       return;
+//     }
+
+//     // Reset the error message and navigate to the credentials page
+//     setErrorMessage('');
+//     navigate('/Groups');
+//   };
+
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: 'url("cer3.jpg")' }}>
+//       <div className="bg-white bg-opacity-50 p-8 rounded-3xl shadow-lg w-full max-w-md border-4 border-green-600 mt-20">
+//         <h1 className="text-3xl font-semibold mb-6 text-center text-green-700">Sign Up</h1>
+
+//         <form className="space-y-6" onSubmit={handleSubmit}>
+//           <div className="grid grid-cols-2 gap-4">
+//             <div>
+//               <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
+//                 First Name
+//               </label>
+//               <input
+//                 type="text"
+//                 id="firstName"
+//                 name="firstName"
+//                 value={firstName}
+//                 onChange={(e) => setFirstName(e.target.value)}
+//                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+//                 Last Name
+//               </label>
+//               <input
+//                 type="text"
+//                 id="lastName"
+//                 name="lastName"
+//                 value={lastName}
+//                 onChange={(e) => setLastName(e.target.value)}
+//                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+//                 Email
+//               </label>
+//               <input
+//                 type="email"
+//                 id="email"
+//                 name="email"
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+//                 Password
+//               </label>
+//               <input
+//                 type="password"
+//                 id="password"
+//                 name="password"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               />
+//             </div>
+//             <div>
+//               <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+//                 Confirm Password
+//               </label>
+//               <input
+//                 type="password"
+//                 id="confirmPassword"
+//                 name="confirmPassword"
+//                 value={confirmPassword}
+//                 onChange={(e) => setConfirmPassword(e.target.value)}
+//                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+//               />
+//             </div>
+//           </div>
+
+//           <div className="mt-4">
+//             <label className="block text-sm font-medium text-gray-700">Gender</label>
+//             <div className="flex items-center mt-2 space-x-8">
+//               <label className="flex items-center text-gray-700">
+//                 <input
+//                   type="radio"
+//                   name="gender"
+//                   value="male"
+//                   checked={gender === 'male'}
+//                   onChange={(e) => setGender(e.target.value)}
+//                   className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300"
+//                 />
+//                 <span className="ml-2">Male</span>
+//               </label>
+//               <label className="flex items-center text-gray-700">
+//                 <input
+//                   type="radio"
+//                   name="gender"
+//                   value="female"
+//                   checked={gender === 'female'}
+//                   onChange={(e) => setGender(e.target.value)}
+//                   className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300"
+//                 />
+//                 <span className="ml-2">Female</span>
+//               </label>
+//             </div>
+//           </div>
+
+//           {errorMessage && (
+//             <div className="text-red-600 text-sm font-medium">
+//               {errorMessage}
+//             </div>
+//           )}
+
+//           <button
+//             type="submit"
+//             className="w-full py-3 px-4 bg-green-600 text-white font-semibold rounded-lg shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+//           >
+//             Register
+//           </button>
+
+//           <div className="text-center text-sm text-gray-700 mt-4">
+//             Already have an account? <a href="/Registrations" className="text-green-600 hover:text-green-500">Log in</a>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SignUp;
+
+
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -10,26 +171,56 @@ const SignUp = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [gender, setGender] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    
-    // Check if all fields are filled
+
+    // Validate form inputs
     if (!firstName || !lastName || !email || !password || !confirmPassword || !gender) {
       setErrorMessage('Please fill out all fields.');
       return;
     }
 
-    // Check if password and confirm password match
     if (password !== confirmPassword) {
       setErrorMessage('Passwords do not match.');
       return;
     }
 
-    // Reset the error message and navigate to the credentials page
-    setErrorMessage('');
-    navigate('/Groups');
+    // Prepare the data to be sent to the server
+    const data = {
+      firstName,
+      lastName,
+      adminEmail: email,
+      adminPassword: password,
+      gender
+    };
+
+    try {
+      const response = await fetch('https://certificatemakerapp-com.stackstaging.com/api/ourProject/signup.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const result = await response.json();
+
+      if (result.status === 'success') {
+        setSuccessMessage('Registration successful. Redirecting to login...');
+        setErrorMessage('');
+        // Redirect to login page or dashboard after a delay
+        setTimeout(() => navigate('/Groups'), 2000);
+      } else {
+        setErrorMessage(result.message || 'An error occurred during registration.');
+        setSuccessMessage('');
+      }
+    // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      setErrorMessage('Failed to connect to the server.');
+    }
   };
 
   return (
@@ -39,66 +230,60 @@ const SignUp = () => {
 
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div className="grid grid-cols-2 gap-4">
+            {/* First Name Field */}
             <div>
-              <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
-                First Name
-              </label>
+              <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name</label>
               <input
                 type="text"
                 id="firstName"
-                name="firstName"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               />
             </div>
+
+            {/* Last Name Field */}
             <div>
-              <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
-                Last Name
-              </label>
+              <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name</label>
               <input
                 type="text"
                 id="lastName"
-                name="lastName"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               />
             </div>
+
+            {/* Email Field */}
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                Email
-              </label>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
               <input
                 type="email"
                 id="email"
-                name="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               />
             </div>
+
+            {/* Password Field */}
             <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password
-              </label>
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
               <input
                 type="password"
                 id="password"
-                name="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               />
             </div>
+
+            {/* Confirm Password Field */}
             <div>
-              <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-                Confirm Password
-              </label>
+              <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
               <input
                 type="password"
                 id="confirmPassword"
-                name="confirmPassword"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 className="mt-1 block w-full px-5 py-3 bg-gray-100 border border-gray-300 rounded-lg text-gray-900 focus:ring-green-500 focus:border-green-500 sm:text-sm"
@@ -106,6 +291,7 @@ const SignUp = () => {
             </div>
           </div>
 
+          {/* Gender Field */}
           <div className="mt-4">
             <label className="block text-sm font-medium text-gray-700">Gender</label>
             <div className="flex items-center mt-2 space-x-8">
@@ -134,12 +320,11 @@ const SignUp = () => {
             </div>
           </div>
 
-          {errorMessage && (
-            <div className="text-red-600 text-sm font-medium">
-              {errorMessage}
-            </div>
-          )}
+          {/* Error and Success Messages */}
+          {errorMessage && <div className="text-red-600 text-sm font-medium">{errorMessage}</div>}
+          {successMessage && <div className="text-green-600 text-sm font-medium">{successMessage}</div>}
 
+          {/* Submit Button */}
           <button
             type="submit"
             className="w-full py-3 px-4 bg-green-600 text-white font-semibold rounded-lg shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
@@ -147,6 +332,7 @@ const SignUp = () => {
             Register
           </button>
 
+          {/* Login Link */}
           <div className="text-center text-sm text-gray-700 mt-4">
             Already have an account? <a href="/Registrations" className="text-green-600 hover:text-green-500">Log in</a>
           </div>
